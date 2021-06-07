8edd25bdd9f1d1adc6e112c70a1cea57cc1fe3c4
aspen <aspenuwu@protonmail.com>
2021-06-07 18:10:54 -0400

    Initial commit

---
+++ libprofile/File.swift
@@ -0,0 +1,9 @@
+
+//
+//  File.swift
+//  libprofile
+//
+//  Created by Aspen on 6/7/21.
+//
+
+import Foundation
---
+++ libprofile/libprofile.h
@@ -0,0 +1,18 @@
+//
+//  libprofile.h
+//  libprofile
+//
+//  Created by Aspen on 6/7/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for libprofile.
+FOUNDATION_EXPORT double libprofileVersionNumber;
+
+//! Project version string for libprofile.
+FOUNDATION_EXPORT const unsigned char libprofileVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libprofile/PublicHeader.h>
+
+
