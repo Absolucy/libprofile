330be772ce340fe144754eb56bc7bac5aa59fc5b
Lucy <lucy@absolucy.moe>
2021-06-24 16:02:17 -0400

    Update FolderFinder, fix extern variables

--- libprofile/Contrib/FolderFinder.swift
+++ libprofile/Contrib/FolderFinder.swift
@@ -11,44 +11,43 @@
 
 import Foundation
 
-@objc class FolderFinder: NSObject {
-
-	@objc class func findSharedFolder(appName: String) -> String? {
+final class FolderFinder: NSObject {
+	
+	final class func findSharedFolder(appName: String) -> String? {
 		let dir = "/var/mobile/Containers/Shared/AppGroup/"
 		return FolderFinder.findFolder(appName: appName, folder: dir)
 	}
-
-	@objc class func findDataFolder(appName: String) -> String? {
+	
+	final class func findDataFolder(appName: String) -> String? {
 		let dir = "/var/mobile/Containers/Data/Application/"
 		return FolderFinder.findFolder(appName: appName, folder: dir)
 	}
-
-	@objc class func findPrivateSharedFolder(appName: String) -> String? {
+	
+	final class func findPrivateSharedFolder(appName: String) -> String? {
 		let dir = "/private/var/mobile/Containers/Shared/AppGroup/"
 		return FolderFinder.findFolder(appName: appName, folder: dir)
 	}
-
-	@objc class func findFolder(appName: String, folder: String) -> String? {
+	
+	final class func findFolder(appName: String, folder: String) -> String? {
 		guard let folders =  try? FileManager.default.contentsOfDirectory(atPath: folder) else { return nil }
-		for folder in folders {
-			let folderPath = folder.appending(folder)
+		for _folder in folders {
+			let folderPath = folder + _folder
 			guard let items = try? FileManager.default.contentsOfDirectory(atPath: folderPath) else { return nil }
 			for itemPath in items {
-				guard let substringRange = itemPath.range(of: ".com.apple.mobile_container_manager.metadata.plist") else {
-					return nil
-				}
-				let range = NSRange(substringRange, in: itemPath)
-				if range.location != NSNotFound {
-					let fullPath = "\(folderPath)/\(itemPath)"
-					let dict = NSDictionary(contentsOfFile: fullPath)
-					if let mcmmetdata = dict?["MCMMetadataIdentifier"] as? NSString,
-					   mcmmetdata.lowercased == appName.lowercased() {
-						return folderPath
+				if let substringRange = itemPath.range(of: ".com.apple.mobile_container_manager.metadata.plist") {
+					let range = NSRange(substringRange, in: itemPath)
+					if range.location != NSNotFound {
+						let fullPath = "\(folderPath)/\(itemPath)"
+						let dict = NSDictionary(contentsOfFile: fullPath)
+						if let mcmmetdata = dict?["MCMMetadataIdentifier"] as? NSString,
+						   mcmmetdata.lowercased == appName.lowercased() {
+							return folderPath
+						}
 					}
 				}
 			}
 		}
 		return nil
 	}
-
+	
 }
--- libprofile/libprofile.h
+++ libprofile/libprofile.h
@@ -9,10 +9,10 @@
 #import "NCNotificationRequest.h"
 
 //! Project version number for libprofile.
-FOUNDATION_EXPORT double libprofileVersionNumber = 1;
+FOUNDATION_EXPORT double libprofileVersionNumber;
 
 //! Project version string for libprofile.
-FOUNDATION_EXPORT const unsigned char libprofileVersionString[] = "1.0.0";
+FOUNDATION_EXPORT const unsigned char libprofileVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <libprofile/PublicHeader.h>
 
